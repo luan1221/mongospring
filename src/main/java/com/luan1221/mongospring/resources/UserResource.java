08c2a9165e6f12935d480da7888d3d9ad0b52c75
luan1221 <lssa@cin.ufpe.br>
2022-05-27 09:39:11 -0300

    user entity and resource

---
+++ src/main/java/com/luan1221/mongospring/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.luan1221.mongospring.resources;
+
+import com.luan1221.mongospring.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(maria, alex)));
+    }
+
+}
