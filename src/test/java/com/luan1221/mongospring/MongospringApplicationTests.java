1fa1eebc16703dd33f5c687ea985cb021f5ff510
luan1221 <lssa@cin.ufpe.br>
2022-05-27 09:21:01 -0300

    first commit

---
+++ src/test/java/com/luan1221/mongospring/MongospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan1221.mongospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
